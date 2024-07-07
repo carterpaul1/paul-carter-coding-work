9a4408769ce3beeac6dd6796a4a79bf413bb606a
paul carter <145701675+carterpaul1@users.noreply.github.com>
2024-07-07 14:01:43 -0400

    Add files via upload
    changed the app to have completed function instead of delete and updated css for button colors and ul font weight for to do items

--- App.js
+++ App.js
@@ -1,25 +1,19 @@
-import React from 'react';
+
 import './App.css';
-import WeatherForm from './WeatherForm';
+import TodoList from './components/TodoList';
 
 function App() {
-  const cityId = '4776024'; // Newport News, Virginia
-
   return (
     <div className="App">
-      <div className="App-header">
+      <header className="App-header">
         <img src="ECPI-Seal.png" alt="ECPI-Seal" />
+      </header>
+      <div className="TodoList">
+       <h2>Todo list </h2>
+        <TodoList />
       </div>
-	  <div>
-      <h1>Every Three Hour forecast for Weather</h1>
-	  <h1>The weather forecast is being pulled through an API Call to openWeathermap.com</h1>
-      <WeatherForm />
     </div>
-	</div>
   );
 }
 
 export default App;
-
-
-
